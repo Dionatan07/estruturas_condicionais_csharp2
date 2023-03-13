62168f7f431d5baa874ff5ecfc56947040b0143a
Dionatan de Andrade <dionatan.andrade@hotmail.com>
2023-03-13 20:25:00 -0300

    Estruturas Condicionais

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿double value = double.Parse(Console.ReadLine());
+
+double imposto;
+
+
+if (value <= 2000){
+    imposto = 0;
+} 
+
+else if (value <= 3000){
+    imposto = (value - 2000) * 0.08;
+}
+else if (value <= 4500){
+    imposto = (value - 3000) * 0.18 + 1000 * 0.08;
+}
+else{
+    imposto = (value - 4500) * 0.28 + 1500 * 0.18 + 1000 * 0.08;
+}
+
+if (imposto == 0.00){
+    Console.WriteLine("Isento");
+}
+else{
+    Console.WriteLine("R$ " + imposto.ToString("F"));
+} 
